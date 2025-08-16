f3e48f7466c6891b0c037819e6ad8a1892b7a962
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2025-08-16 16:11:47 -0600

    Change cookies to Firefox

--- scripts/download_videos.zsh
+++ scripts/download_videos.zsh
@@ -13,9 +13,9 @@ cd "$DOWNLOAD_DIR" || exit 1
 if [[ "$URL" == *"playlist"* || "$URL" == *"&list="* ]]; then
   echo "Detected playlist."
   yt-dlp -f "bestvideo+bestaudio/best" --yes-playlist -o "%(playlist_index)s - %(title)s.%(ext)s" "$URL"
-elif [[ "$URL" == *"x.com"* ]]; then
-  echo "Detected Twitter."
-  yt-dlp --cookies-from-browser chrome "$URL"
+elif [[ "$URL" != *"youtube.com"* ]]; then
+  echo "Detected Non-YouTube."
+  yt-dlp --cookies-from-browser firefox "$URL"
 else
   echo "Detected single video."
   yt-dlp "$URL"
